fd6f1ba4fd7c6f25151ebfd287024513cf75d33b
Ganesh <ganesh.birajdar.1930@gmail.com>
2022-01-20 21:51:11 +0530

    google-docs-clone

--- projbackend/server.js
+++ projbackend/server.js
@@ -1,38 +1,53 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const Document = require("./models/Document");
+
+// DB CONNECTION
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// we need cors , our client and server both are on diff urls
+// cors helps in making reqs
 const io = require("socket.io")(8080, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: "http://localhost:3000", // where our client is
+    methods: ["GET", "POST"], // methods allowed
   },
 });
 
 const defaultValue = "";
 
+// everytime client connects
 io.on("connection", (socket) => {
+  // grabbing the docId from client
   socket.on("get-document", (documentId) => {
     const document = findOrCreateDocument(documentId);
-    socket.join(documentId);
+
+    // putting user in one room
+    socket.join(documentId); // putting this socket in a specific room
     socket.emit("load-document", document.data);
+
+    // getting changes from client
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
     });
+
+    //saving the doc
     socket.on("save-document", async (data) => {
       await Document.findByIdAndUpdate(documentId, { data });
     });
   });
 });
 
+// whenever we get a docId
 const findOrCreateDocument = async (id) => {
   if (id == null) return;
 
   const document = await Document.findById(id);
   if (document) return document;
+
+  //by default if we dont have a doc we are going just make one
   return await Document.create({ _id: id, data: defaultValue });
 };
--- projfrontend/src/components/TextEditor.js
+++ projfrontend/src/components/TextEditor.js
@@ -1,6 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
+
+// stylesheet for quill (snow theme)
 import "quill/dist/quill.snow.css";
+
+//client version of socket io
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
 require("dotenv").config();
@@ -18,31 +22,40 @@ const TOOLBAR_OPTIONS = [
 ];
 
 const TextEditor = () => {
-  const { id: documentId } = useParams();
+  const { id: documentId } = useParams(); //renaming as documentId
   const [socket, setSocket] = useState();
   const [quill, setQuill] = useState();
   console.log(documentId);
 
+  //
   useEffect(() => {
     const s = io("http://localhost:8080");
     setSocket(s);
+
     return () => {
+      //calling disconnect function
       s.disconnect();
     };
   }, []);
 
+  // making sure the changes occur on that specific doc-id
   useEffect(() => {
     if (socket == null || quill == null) return;
+
+    // once cleans up after running once
     socket.once("load-document", (document) => {
       quill.setContents(document);
       quill.enable();
     });
 
+    // send doc-id to the server
     socket.emit("get-document", documentId);
   }, [socket, quill, documentId]);
 
+  // saving the doc
   useEffect(() => {
     if (socket == null || quill == null) return;
+
     const interval = setInterval(() => {
       socket.emit("save-document", quill.getContents());
     }, process.env.SAVE_INTERVAL_MS);
@@ -51,8 +64,11 @@ const TextEditor = () => {
     };
   }, [socket, quill]);
 
+  // on receiving changes
   useEffect(() => {
     if (socket == null || quill == null) return;
+
+    // running the changes (updating the doc)
     const handler = (delta, oldDelta, source) => {
       quill.updateContents(delta);
     };
@@ -61,12 +77,20 @@ const TextEditor = () => {
       socket.off("receive-changes", handler);
     };
   }, [socket, quill]);
+
+  // detecting text-change on quill changes
   useEffect(() => {
+    // making sure we have socket and quill
     if (socket == null || quill == null) return;
+
+    // read the docs
     const handler = (delta, oldDelta, source) => {
+      // changes should be done by the user
       if (source !== "user") return;
+      // emit msg from client to server - delta is that small subset of whats changed in the doc and not the whole doc
       socket.emit("send-changes", delta);
     };
+
     quill.on("text-change", handler);
     return () => {
       quill.off("text-change", handler);
@@ -75,9 +99,12 @@ const TextEditor = () => {
 
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
-    wrapper.innerHTML = "";
+    wrapper.innerHTML = ""; // to avoid multiple toolbars
+
     const editor = document.createElement("div");
     wrapper.append(editor);
+
+    //new instance of the quill editor (inside useCall coz we want it to render only once)
     const q = new Quill(editor, {
       theme: "snow",
       modules: { toolbar: TOOLBAR_OPTIONS },
